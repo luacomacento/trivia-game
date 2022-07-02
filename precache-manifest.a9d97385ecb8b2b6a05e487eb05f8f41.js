917e80ee7220da841bc0cf49a4c9ed9ada237e0d
Luá <luacomacento@gmail.com>
2022-07-02 15:34:56 -0300

    Updates

---
+++ precache-manifest.a9d97385ecb8b2b6a05e487eb05f8f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f22e4c38ca1fd91017453729b24b09",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "725a64d48ce161fae62f",
+    "url": "/trivia-game/static/css/main.85ec6247.chunk.css"
+  },
+  {
+    "revision": "f687c7db291b15f2bcbe",
+    "url": "/trivia-game/static/js/2.ac7b24f9.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia-game/static/js/2.ac7b24f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725a64d48ce161fae62f",
+    "url": "/trivia-game/static/js/main.d954be03.chunk.js"
+  },
+  {
+    "revision": "b0c6c90c513f51408a18",
+    "url": "/trivia-game/static/js/runtime-main.4bc9de50.js"
+  }
+]);
